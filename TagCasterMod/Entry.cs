9b24bc85539c6f7c5a622240abe2e5b83847fc5b
Tiyenti <11019859+Tiyenti@users.noreply.github.com>
2021-12-05 01:12:45 +0000

    initial commit

---
+++ TagCasterMod/Entry.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Reactor.API;
+using Reactor.API.Attributes;
+using Reactor.API.Interfaces.Systems;
+using UnityEngine;
+using Centrifuge.Distance.Game;
+
+namespace TagCasterMod
+{
+    [ModEntryPoint("com.github.tiyenti/TagCasterMod")]
+    public class Entry : MonoBehaviour
+    {
+        UILabel watermark = null;
+
+        bool showDataInWatermark = true;
+
+        public void Initialize(IManager manager)
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        bool firstMenuLoad = true;
+
+        public void LateInitialize(IManager manager)
+        {
+            Events.MainMenu.Initialized.Subscribe((data) =>
+            {
+                if (firstMenuLoad)
+                {
+                    watermark = GameObject.Find("AlphaVersion").GetComponent<UILabel>();
+                    watermark.width = 500;
+                    watermark.enabled = true;
+                    watermark.effectColor = new Color(0, 0, 0, 1);
+                    watermark.effectStyle = UILabel.Effect.Outline ;
+                    firstMenuLoad = false;
+                }
+                else
+                {
+                    
+                }
+
+                watermark.text = "[c][00FF8C]TAG CASTER MOD ENABLED[-][/c]";
+            });
+
+            Events.Scene.BeginSceneSwitchFadeOut.Subscribe((data) =>
+            {
+                if (watermark != null)
+                {
+                    watermark.text = "";
+                }
+            });
+        }
+
+        void Update()
+        {
+            if (G.Sys.PlayerManager_?.Current_?.playerData_?.Finished_ == true &&
+                G.Sys.GameManager_?.Mode_?.IsStarted_ == true &&
+                G.Sys.NetworkingManager_.IsOnline_)
+            //if (G.Sys.GameManager_?.Mode_?.IsStarted_ == true && G.Sys.NetworkingManager_.IsOnline_)
+            {
+                if (G.Sys.PlayerManager_?.Current_?.playerData_?.CarCamera_?.HasComponent<SpectatorCameraLogic>() == true && showDataInWatermark)
+                //if (showDataInWatermark)
+                {
+                    watermark.enabled = true;
+
+                    var watermarkTextToSetTo = "[c][ffffff]";
+
+                    var list = new List<ModePlayerInfoBase>();
+                    G.Sys.GameManager_.Mode_.GetSortedListOfModeInfosNotFinished(list);
+                    var index = 0;
+                    foreach (var a in list)
+                    {
+                        if (G.Sys.GameManager_.ModeID_ == GameModeID.ReverseTag)
+                        {
+                            if (a == ((ReverseTagMode)G.Sys.GameManager_.Mode_).TaggedPlayer_)
+                            {
+                                watermarkTextToSetTo += "[D1A7FF]";
+                            }
+                            else
+                            {
+                                watermarkTextToSetTo += "[ffffff]";
+                            }
+                        }
+
+                        watermarkTextToSetTo += $"{a.Name_}[-] | {a.pData_.PrimaryColor_.ToFormattedHex()}█[-]" +
+                                                $"{a.pData_.SecondaryColor_.ToFormattedHex()}█[-]" +
+                                                $"{a.pData_.OriginalGlowColor_.ToFormattedHex()}█[-]" ;
+
+                        if (G.Sys.GameManager_.ModeID_ == GameModeID.ReverseTag)
+                        {
+                            watermarkTextToSetTo += $" | {GUtils.GetFormattedTime(a.modeData_, true)}";
+                        }
+                        else if (G.Sys.GameManager_.ModeID_ == GameModeID.Sprint)
+                        {
+                            watermarkTextToSetTo += $" | {GUtils.GetFormattedDistance(a.modeData_)}";
+                        }
+                        else
+                        {
+                            watermarkTextToSetTo += $" | {a.modeData_}";
+                        }
+
+                        if (index == 0)
+                        {
+                            watermarkTextToSetTo += " [EFD034]| 1[-]";
+                        }
+                        else if (index == 1)
+                        {
+                            watermarkTextToSetTo += " [A0A0A0]| 2[-]";
+                        }
+                        else if (index == 2)
+                        {
+                            watermarkTextToSetTo += " [BC5C1C]| 3][-]";
+                        }
+                        else
+                        {
+                            watermarkTextToSetTo += $"| {index+1}";
+                        }
+                        watermarkTextToSetTo += "[/c]\n";
+                        index++;
+                    }
+
+                    watermark.text = watermarkTextToSetTo;
+                }
+            }
+        }
+    }
+}
