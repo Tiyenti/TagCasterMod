85a887c27d31c36599971b9836a9d1b246159cb2
Tiyenti <11019859+Tiyenti@users.noreply.github.com>
2022-02-23 15:40:06 +0000

    try making a fake car

--- TagCasterMod/DualSpectateMode.cs
+++ TagCasterMod/DualSpectateMode.cs
@@ -25,14 +25,24 @@ namespace TagCasterMod
             Console.WriteLine("Dual spectate mode is being activated");
             dualSpectateActive = true;
 
-            var secondCamObj = UnityEngine.Object.Instantiate(initialCarCam.gameObject);
-            secondCam = secondCamObj.GetComponent<Camera>();
+            //var secondCamObj = UnityEngine.Object.Instantiate(initialCarCam.gameObject);
+            //var secondCamObj = new GameObject();
+            //var secondCam = secondCamObj.AddComponent<Camera>();
+
+            var pdf = PlayerDataFake.InitializeFakePlayerData(G.Sys.ProfileManager_.CurrentProfile_, false);
+            pdf.carCamera_.isSpectating_ = true;
+            pdf.carCamera_.camera_.cullingMask |= 1 << 1;
+
+            pdf.carCamera_.GetOrAddComponent<SpectatorCameraLogic>();
+
 
             initialCarCam.camera_.rect = new Rect(0, 0, 0.5f, 1);
-            secondCam.rect = new Rect(1f, 0, 0.5f, 1);
+            pdf.CarCamera_.camera_.rect = new Rect(0.5f, 0, 0.5f, 1);
 
-            secondCamObj.RemoveComponent<AdjustRadialBlur>();
-            secondCamObj.RemoveComponent<PlayerSpecificRenderingCamera>();
+            
+
+            //secondCamObj.RemoveComponent<AdjustRadialBlur>();
+            //secondCamObj.RemoveComponent<PlayerSpecificRenderingCamera>();
 
         }
 
@@ -40,7 +50,24 @@ namespace TagCasterMod
         {
             if (secondCam && dualSpectateActive)
             {
-                secondCam.Render();
+                /*List<PlayerDataBase> a = new List<PlayerDataBase>();
+                if (G.Sys.NetworkingManager_.IsOnline_)
+                {
+                    G.Sys.GameManager_.Mode_.GetSortedListOfUnfinishedPlayers(a);
+                }
+                else
+                {
+                    foreach (var b in PlayerDataReplay.ReplayPlayers_)
+                    {
+                        if (b != null && !b.Finished_)
+                        {
+                            a.Add(b);
+                        }
+                    }
+                }
+
+                secondCam.transform.position = a[0].transform.position;*/
+                
             }
         }
     }
